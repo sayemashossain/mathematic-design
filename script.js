71c68e5a78922d6dfb531c18b3b25784014f6286
sayemashossain <sayemashossain@gmail.com>
2021-12-31 02:01:34 +0600

    Added random generation of lines and colours

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
-var canvasWidth = document.querySelector("#canvasDiv").offsetWidth;
-var canvasHeight = document.querySelector("#canvasDiv").offsetHeight;
+var i;
+var lineInterval = 30;
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-var o = window.document.getElementById("canvasDiv");
-o.innerHTML="<canvas id='myCanvas' width='"+canvasWidth+"' height='"+canvasHeight+"'></canvas>";
-console.log(canvasWidth + " " + canvasHeight);
+var bgColour = randomColor.substr(-2, 2) + randomColor.substring(0,4);
+console.log(randomColor);
+console.log(bgColour);
+
+
+
+
+var canvasWidth = document.querySelector("#canvasDiv").offsetWidth;//(document.querySelector("#canvasDiv").offsetWidth) - ((document.querySelector("#canvasDiv").offsetWidth) % lineInterval);
+var canvasHeight = document.querySelector("#canvasDiv").offsetHeight; //(document.querySelector("#canvasDiv").offsetHeight) - ((document.querySelector("#canvasDiv").offsetHeight) % lineInterval);
+
+var canvasDiv = window.document.getElementById("canvasDiv");
+canvasDiv.innerHTML = "<canvas id='myCanvas' width='" + canvasWidth + "' height='" + canvasHeight + "'></canvas>";
+// console.log(canvasWidth + " " + canvasHeight);
+
+document.querySelector("#canvasDiv").style.backgroundColor = bgColour;
+
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-var i;
-for (i = 0; i < canvasWidth; i += 30) {
-    for (j = 0; j < canvasHeight; j += 30) {
 
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 5;
 
-        if (i % 60 == 0) {
-            ctx.beginPath();
-            ctx.moveTo(i, j);
-            ctx.lineTo((i + 30), j);
-            ctx.stroke();
 
-            ctx.beginPath();
-            ctx.moveTo(i, j);
-            ctx.lineTo(i, (j + 30));
-            ctx.stroke();
+ctx.strokeStyle = "#" + randomColor; //"green";
+ctx.lineWidth = 10;
+ctx.lineCap = "round";
 
-        }
-        else {
-            ctx.beginPath();
-            ctx.moveTo(i, j);
-            ctx.lineTo((i + 30), j);
-            ctx.stroke();
+for (i = 0; i < canvasWidth; i += lineInterval) {
+    var rand = getRndInteger(0, 1);
+    var j;
+    if (rand == 0) { j = 0; }
+    else { j = lineInterval; }
 
-            ctx.beginPath();
-            ctx.moveTo(i, j);
-            ctx.lineTo(i, (j + 30));
-            ctx.stroke();
-
-        }
+    for (; j < canvasHeight; j += lineInterval * 2) {
+        ctx.beginPath();
+        ctx.moveTo(i, j);
+        ctx.lineTo(i, (j + lineInterval));
+        ctx.stroke();
     }
 }
+
+
+for (j = 0; j < canvasHeight; j += lineInterval) {
+    var rand = getRndInteger(0, 1);
+    var i;
+    if (rand == 0) { i = 0; }
+    else { i = lineInterval; }
+
+    for (; i < canvasWidth; i += lineInterval * 2) {
+        ctx.beginPath();
+        ctx.moveTo(i, j);
+        ctx.lineTo((i + lineInterval), j);
+        ctx.stroke();
+    }
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
